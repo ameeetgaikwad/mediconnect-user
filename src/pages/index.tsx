ac19d6964f2107587d66745ce1c693e76c177e61
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2023-11-11 22:12:29 +0530

    style: home page responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-row items-center justify-between p-24 ${inter.className} bg-[url(/assets/bg.jpg)] bg-center bg-cover justify-around`}
-    >
+    <main className="flex min-h-screen flex-col items-center p-24  bg-[url(/assets/bg.jpg)] bg-center bg-cover justify-around sm:flex-row sm:items-center sm:justify-around">
       <Head>
         <title>Mediconnect</title>
       </Head>
@@ -19,7 +17,15 @@ export default function Home() {
         <p className="text-xl font-extrabold">Coming soon...</p>
       </div>
 
-      <Image src={"/assets/asset.svg"} width={500} height={500} alt="asset" />
+      <div className="w-screen">
+        <Image
+          src={"/assets/asset.svg"}
+          width={400}
+          height={400}
+          alt="asset"
+          className="w-[100%] h-[100%]"
+        />
+      </div>
     </main>
   );
 }
