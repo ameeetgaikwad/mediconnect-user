7b730cc59865f5032d2fed8e07b425d085c28725
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-05-30 10:31:55 +0530

    pg

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 type Data = {
-  name: string
-}
+  email: string;
+  status: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { email } = req.body;
+  const allUsers = await prisma.user.create({
+    data: {
+      email: email,
+    },
+  });
+
+  res.status(200).json({ email: email, status: "success" });
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,23 @@ import Head from "next/head";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useEffect } from "react";
 import Link from "next/link";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { status, data } = useSession();
+  const { data: session } = useSession();
 
   useEffect(() => {
-    console.log(data);
-  }, []);
-  console.log(status);
+    if (status === "authenticated") {
+      registerUser();
+    }
+    console.log(session);
+  }, [status]);
+  async function registerUser() {
+    await axios.post("/api/hello", { email: session?.user?.email });
+  }
   return (
     <main className="flex min-h-screen flex-col items-center p-24  bg-[url(/assets/bg.jpg)] bg-center bg-cover justify-around sm:flex-row sm:items-center sm:justify-around">
       <Head>
@@ -23,7 +30,13 @@ export default function Home() {
         <h1 className="text-5xl font-bold uui-gradient-text pb-4">
           Medconnect
         </h1>
-        <p className="text-xl font-extrabold">Coming soon...</p>
+        <Link
+          className="text-xl font-extrabold bg-purple-700 p-2 rounded-xl"
+          href={"https://www.google.com"}
+          target="blank"
+        >
+          Download Now
+        </Link>
       </div>
 
       <div className="w-screen sm:w-[32%]">
@@ -39,7 +52,7 @@ export default function Home() {
         href={"/auth/signin"}
         className="absolute top-6 right-8 uui-gradient-bg text-center w-[15%] h-[6%] sm:w-[6%] sm:h-[6%] flex felx-row items-center justify-center rounded-3xl"
       >
-        Login
+        Register
       </Link>
     </main>
   );
